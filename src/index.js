d968460e0454d86009e8574a87edf21b59074358
til2to <padwusup@gmail.com>
2023-05-16 01:28:44 +0000

    create leader board layout and use dummy data

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+import viewScoreBoard from './modules/scoreView.js';
+
+const playerScores = [
+  { name: 'name', score: 100 },
+  { name: 'name', score: 20 },
+  { name: 'name', score: 50 },
+  { name: 'name', score: 78 },
+  { name: 'name', score: 125 },
+  { name: 'name', score: 77 },
+  { name: 'name', score: 42 },
+];
+
+viewScoreBoard(playerScores);
