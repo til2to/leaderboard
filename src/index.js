5f8a4c56475bf617affe5a011256649d6e04ba94
til2to <padwusup@gmail.com>
2023-05-16 02:18:12 +0000

    remove scoreview file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
-import viewScoreBoard from './modules/scoreView.js';
+// import viewScoreBoard from './modules/scoreView.js';
 
+const scoreContainer = document.querySelector('.player-score-container');
 const playerScores = [
   { name: 'name', score: 100 },
   { name: 'name', score: 20 },
@@ -11,4 +12,17 @@ const playerScores = [
   { name: 'name', score: 42 },
 ];
 
-viewScoreBoard(playerScores);
+// dynamically populate todo player name and score
+const viewScoreBoard = (playerScores) => {
+  let score = '';
+  playerScores.forEach((player) => {
+    score += `
+      <li class="each-player">
+        ${player.name}: ${player.score}
+      </li>
+    `;
+  });
+  scoreContainer.innerHTML = score;
+};
+
+viewScoreBoard();
